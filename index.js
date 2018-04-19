954a01e9c944f4acebba2d8cdb0f9b8e4bf2fe3e
TmothyMartel <t.m.martel@gmail.com>
2018-04-19 10:29:27 -0400

    desktop styles complete

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 // $.ajax({
 //    url: "https://www.googleapis.com/books/v1/volumes",
 //    data: {
-//       q: "lord of the rings"
+//       q: "neuromancer"
+      
 //    },
 //    error: function(error) {
 //       console.log('error', error);
@@ -14,28 +15,22 @@
 //    type: 'GET'
 // });
 
+// function showTasteDiveResults(data) {
+//   console.log(data);
+// }
+
 
 // $.ajax({
 //   url: "https://tastedive.com/api/similar?",
-//   //header: {  'Access-Control-Allow-Origin': 'https://null.jsbin.com/'},
 //   data: {
 //    q: "lord of the rings",
 //    type: "books",
 //    info: 1,
 //    limit: 10,
 //    k: "304808-authorse-BD8LBICQ",
-//    callback: { 
-//        error: function(error) {
-//          console.log('error', error);
-//      },
-//       success: function(data) {
-//        console.log('success', data);
-//      }
-//    },
-//     crossDomain: true,
-//    dataType: 'jsonp',
-//    method: 'GET'
-//  }
+//   },
+//   jsonpCallback: "showTasteDiveResults",
+//   dataType: 'jsonp',
 // });
 
 
