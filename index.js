c47be0bd26091eb3bd9b1fdbe60885e4972910ca
TmothyMartel <t.m.martel@gmail.com>
2018-05-01 11:53:59 -0400

    added ability to navigate to book-view from search results

--- index.js
+++ index.js
@@ -1,74 +1,93 @@
 'use strict'
 
 function googleApiSearch(searchTerm, callback) {
-$.ajax({
-    url: "https://www.googleapis.com/books/v1/volumes",
-    data: {
-        q: `${searchTerm}`,
-        maxResults: 6,
-        orderBy: 'relevance'
-    },
-    error: function(error) {
-        console.log('error', error);
-    },
-    success: function(data) {
-        showGoogleBooksResults(data);
-    },
-    type: 'GET'
-});
+    $.ajax({
+        url: "https://www.googleapis.com/books/v1/volumes",
+        data: {
+            q: `${searchTerm}`,
+            maxResults: 6,
+            orderBy: 'relevance'
+        },
+        error: function(error) {
+            console.log('error', error);
+        },
+        success: function(data) {
+            showGoogleBooksResults(data);
+        },
+        type: 'GET'
+    });
 }
 
 function tastediveApiSearch(tastediveSearchTerm) {
-$.ajax({
-  url: "https://tastedive.com/api/similar?",
-  data: {
-   q: `${tastediveSearchTerm}`,
-   type: "books",
-   info: 1,
-   limit: 10,
-   k: "304808-authorse-BD8LBICQ",
-  },
-  jsonpCallback: `showTasteDiveResults`,
-  dataType: 'jsonp',
-});
+    $.ajax({
+        url: "https://tastedive.com/api/similar?",
+        data: {
+            q: `${tastediveSearchTerm}`,
+            type: "books",
+            info: 1,
+            limit: 10,
+            k: "304808-authorse-BD8LBICQ",
+        },
+        jsonpCallback: `showTasteDiveResults`,
+        dataType: 'jsonp',
+    });
 }
 
 function resultsRender(result) {
-    $('.list-book-cover').attr('src', `${result.volumeInfo.imageLinks.thumbnail}`);
-    $('.list-book-title').text(`${result.volumeInfo.title}`);
-    $('.list-book-synopsis').text(`${result.volumeInfo.description}`);
+    // $('.list-book-cover').attr('src', `${result.volumeInfo.imageLinks.thumbnail}`);
+    // $('.list-book-title').text(`${result.volumeInfo.title}`);
+    // $('.list-book-synopsis').text(`${result.volumeInfo.description}`);
+    return `
+          <li>
+              <button id="js-book-view" class="book-info-link">
+              <img class="list-book-cover js-book-view-link" src="${result.volumeInfo.imageLinks.thumbnail}" alt="image of book's cover">
+              </button>
+              <p class="list-book-title">${result.volumeInfo.title}</p>
+              <p class="list-book-synopsis">${result.volumeInfo.description}</p>
+          </li>
+          `
 }
 
 function bookInfoViewRender(result) {
-  $('.book-cover').attr('src', `${result.imageLinks.thumbnail}`);
-  $('.book-title').text(`${result.title}`);
-  $('.book-author').text(`${result.authors}`);
-  $('#book-description').text(`${result.description}`);
-  $('.book-publisher').text(`${result.publisher}`);
-  $('.book-pages').text(`${result.pageCount}`);
-  $('.book-category').text(`${result.categories[0]}`);
+    $('.book-cover').attr('src', `${result.imageLinks.thumbnail}`);
+    $('.book-title').text(`${result.title}`);
+    $('.book-author').text(`${result.authors}`);
+    $('#book-description').text(`${result.description}`);
+    $('.book-publisher').text(`${result.publisher}`);
+    $('.book-pages').text(`${result.pageCount}`);
+    $('.book-category').text(`${result.categories[0]}`);
 }
 
-                
-               
+function showBookView() {
+  console.log("ready")
+$('#js-book-view').on('click', event => {
+   event.preventDefault();
+    $('.book-view').show();
+    $('.home-view').hide();
+    $('.search-result-view').hide();
+});
+}
+
+
 function showGoogleBooksResults(data) {
     const bookViewResult = data.items[0].volumeInfo;
     bookInfoViewRender(bookViewResult);
     const results = data.items.map((item, index) => resultsRender(item));
-  $('.results').prop('hidden', false).html(results);
+    $('.results').prop('hidden', false).append(results);
+    showBookView();
 }
 
-function tastediveRender(item) {
-   <p class="book-title">${result.Similar.Info[0].Name}</p>
-  <p class="book-description">${result.Similar.Info[0].wTeaser}</p>
+// function tastediveRender(item) {
+//    <p class="book-title">${result.Similar.Info[0].Name}</p>
+//   <p class="book-description">${result.Similar.Info[0].wTeaser}</p>
 
-}
+// }
 
 // function showTasteDiveResults(data) {
 //   const recommendations = tastediveRender(data);
 //   console.log(results);
 //   //.map((item, index) => resultsRender(item));
- 
+
 // }
 
 
@@ -76,35 +95,37 @@ function tastediveRender(item) {
 
 
 function userSearchEventListener() {
-    $('#js-search-form').on('click', 'button.search.button', event => {
+    $('#js-search-form').on('click', 'button.search.btn', event => {
         event.preventDefault();
         const queryTarget = $('#js-search-form').find('input.js-search-bar');
         const query = queryTarget.val();
         queryTarget.val("");
         googleApiSearch(query, showGoogleBooksResults);
+        $('.search-result-view').show();
+        $('.home-view').hide();
         //tastediveApiSearch(query);
 
     });
 }
 
-$(userSearchEventListener());
+function handleEventListeners() {
+  userSearchEventListener();
+  
+}
+
+$(handleEventListeners);
 
 
 
-// $('#js-home-view').on('click', function() {
-// 		$('.home-view').show();
-// 		$('.book-view').hide();
-// 		$('.recommend-view').hide();
-// 	});
+$('#js-home-view').on('click', function() {
+    $('.home-view').show();
+    $('.book-view').hide();
+    $('.search-result-view').hide();
+});
 
-// $('#js-book-view').on('click', function() {
-// 		$('.book-view').show();
-// 		$('.home-view').hide();
-// 		$('.recommend-view').hide();
-// 	});
 
 // $('#js-recommend-view').on('click', function() {
-// 		$('.recommend-view').show();
-// 		$('.home-view').hide();
-// 		$('.book-view').hide();
-// 	});
+//     $('..search-result-view').show();
+//     $('.home-view').hide();
+//     $('.book-view').hide();
+// });
